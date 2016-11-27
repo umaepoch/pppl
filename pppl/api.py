67b391a7038315da61142f61b0e0292b5bf83ed8
Uma <umag@epochconsulting.in>
2016-11-27 14:15:12 +0530

    API

--- pppl/api.py
+++ pppl/api.py
@@ -4,6 +4,8 @@ import frappe
 from frappe.utils import cint, flt, cstr, comma_or
 from erpnext.setup.utils import get_company_currency
 from frappe import _, throw
+from frappe.model.mapper import get_mapped_doc
+
 
 
 @frappe.whitelist()
@@ -27,3 +29,40 @@ def set_total_in_words(doc, method):
         doc.vat_in_words = money_in_words(disable_rounded_total and
             abs(doc.vat_amount) or abs(doc.vat_amount), doc.currency)
 
+
+@frappe.whitelist()
+def make_purchase_receipt(source_name, target_doc=None):
+	def update_item(obj, target, source_parent):
+		target.qty = flt(obj.qty) 
+		target.stock_qty = flt(obj.qty)
+		target.amount = flt(obj.qty) * flt(obj.rate)
+		target.base_amount = flt(obj.qty) * flt(obj.rate) 
+
+	doc = get_mapped_doc("Sales Invoice", source_name,	{
+		"Sales Invoice": {
+			"doctype": "Purchase Receipt",
+			"validation": {
+				"docstatus": ["=", 1],
+			}
+		},
+		"Sales Invoice Item": {
+			"doctype": "Purchase Receipt Item",
+			"field_map": {
+				"name": "sales_invoice_item",
+				"parent": "sales_invoice",
+			},
+			"postprocess": update_item,
+			"condition": lambda doc: abs(doc.qty) > 0
+		},
+		"Purchase Taxes and Charges": {
+			"doctype": "Purchase Taxes and Charges",
+			"add_if_empty": True
+		}
+	}, target_doc, set_missing_values)
+
+	return doc
+
+def set_missing_values(source, target):
+	target.ignore_pricing_rule = 1
+	target.run_method("set_missing_values")
+	target.run_method("calculate_taxes_and_totals")
